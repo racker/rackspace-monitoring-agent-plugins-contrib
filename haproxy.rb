67aea943400e77a8169ab9f12127d5d23920ff93
Jay Faulkner <jay@jvf.cc>
2013-05-17 10:03:01 -0700

    update(haproxy.rb): Update status strings for use with new agent
    - Now compliant with
      http://docs.rackspace.com/cm/api/v1.0/cm-devguide/content/appendix-check-types-agent.html#section-ct-agent.plugin

--- haproxy.rb
+++ haproxy.rb
@@ -1,59 +1,87 @@
 #!/usr/bin/env ruby
-
-# because CloudKick wants a license to merge...
 # ----------------------------------------------------------------------------
 # "THE BEER-WARE LICENSE" (Revision 42):
 # <lo@petalphile.com> wrote this file. As long as you retain this notice you
 # can do whatever you want with this stuff. If we meet some day, and you think
 # this stuff is worth it, you can buy me a beer in return 
 # ----------------------------------------------------------------------------
-
-### the script:
-
-# takes HAProxy stats and grabs connections, rate, and check time
-# for every listener and every backend server, using a format like
-# "metric groupname_servername_request_rate int 10
-
-# variables you may actually need to change
-
-# change this if the file is elsewhere
-config	= "/etc/haproxy/haproxy.cfg" || raise("Expecting haproxy configuration file in /etc/haproxy/haproxy.cfg")
-
-# grab the statistics socket from above
-socket	= `awk '/stats socket/ {print $3}' #{config}`.chomp || raise("Expecting \'stats socket <UNIX_socket_path>\' in #{config}")
-
-# where haproxy lives and pid (either in conf or static location)
-exec    = `which haproxy`.chomp || raise("Where the F is haproxy?")
-pid	= `pidof haproxy`.chomp.to_i || nil
+#
+# haproxy.rb
+#  - Takes HAProxy stats and grabs connections, rate, and check time
+#    for every listener and every backend server, and prints it using
+#    Rackspace Cloud Montioring metric lines
 
 
-
-if ( pid )
-	puts "status ok haproxy is running"
-	conn = `lsof -ln -i |grep -c #{pid}`.chomp.to_i
-# removes the listener and stats socket
-	conn = conn - 2
-	puts "metric connections int #{conn}"
-# grab statistics from the socket
-
-	require 'socket'
-
-	ctl=UNIXSocket.new(socket)
-	ctl.puts "show stat"
-
-	while (line = ctl.gets) do
-		if (line =~ /^[^#]\w+/)
-			line = line.split(",")
-			host = "#{line[0]}_#{line[1]}"
-			puts "metric #{host}_request_rate int #{line[47]}" if line[47].to_i > 0
-			puts "metric #{host}_total_requests gauge #{line[49]}" if line[49].to_i > 0
-			puts "metric #{host}_health_check_duration int #{line[35]}" if line[35].to_i > 0
-			puts "metric ${host}_current_queue int #{line[3]}" if line[3].to_i > 0
-		end
-	end
-
-	ctl.close
-
-else
-	puts "status err haproxy is not running!"
+def fail(status="Unknown failure")
+  puts "status #{status}"
+  exit 1
 end
+
+def metric(name,type,value)
+  @metrics[name] = {
+    :type => type,
+    :value => value
+  }
+end
+
+def output_success
+  puts "status HAProxy is running and reporting metrics!"
+  @metrics.each do |name,v|
+    puts "#{name} #{v[:type]} #{v[:value]}"
+  end
+end
+
+begin
+  require 'optparse'
+  require 'socket'
+rescue
+  fail "Failed to load required ruby gems!"
+end
+
+@metrics = {}
+options = {}
+
+args = ARGV.dup
+
+OptionParser.new do |o|
+  o.banner = "Usage: check_agent_public.rb [options]"
+  o.on('-s', '--stats-socket SOCKET', 'Specify the HAProxy stats socket') do |s| 
+    options[:sock] = s 
+  end
+  o.on_tail('-h', '--help', 'Show this message') { puts o; exit }
+  o.parse!(args)
+end
+
+fail "You must specify the haproxy stats socket!" if options[:sock].nil?
+
+pid = `pidof haproxy`.chomp.to_i || fail("status HAProxy is not running!")
+
+begin
+  conn = `lsof -ln -i |grep -c #{pid}`.chomp.to_i
+  # removes the listener and stats socket
+  conn = conn - 2
+  metric("connections","int",conn.to_i)
+rescue
+  fail "Error while getting connection counts!"
+end
+
+begin
+  ctl=UNIXSocket.new(options[:sock])
+  ctl.puts "show stat"
+
+  while (line = ctl.gets) do
+    if (line =~ /^[^#]\w+/)
+      line = line.split(",")
+      host = "#{line[0]}_#{line[1]}"
+      metric("#{host}_request_rate","int",line[47].to_i)
+      metric("#{host}_total_requests","gauge",line[49].to_i)
+      metric("#{host}_health_check_duration","int",line[35].to_i)
+      metric("#{host}_current_queue","int",line[3].to_i)
+    end
+  end
+  ctl.close
+rescue
+  fail "Error while reading from #{options[:sock]}!"
+end
+
+output_success
