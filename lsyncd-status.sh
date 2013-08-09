732051b91c8953431e9b071ca9f4259f8609ba4d
lindseyanderson <lindsey.anderson@rackspace.com>
2013-08-09 13:50:06 -0400

    Update lsyncd-status.sh
    Updated comments

--- lsyncd-status.sh
+++ lsyncd-status.sh
@@ -28,8 +28,6 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 #
-# See https://github.com/robszumski/rackspace-monitoring-varnish for a readme
-# and more information
 #
 # Verify the current status of lsyncd
 # *****************
