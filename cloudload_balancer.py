259dc745223cc52dd722b857972512eb7dcaaf94
Ryan Phillips <ryan.phillips@rackspace.com>
2013-09-10 17:47:00 -0500

    add stats for online, offline, and draining

--- cloudload_balancer.py
+++ cloudload_balancer.py
@@ -44,6 +44,7 @@ def check_usage(region):
     for instance in clb.list():
         mgr = instance.manager
         status = instance.status
+        nodes = instance.nodes
         name = instance.name.lower().replace('-', '_')
         usage = mgr.get_usage(instance)
         usage = usage['loadBalancerUsageRecords'][-1]
@@ -56,6 +57,25 @@ def check_usage(region):
         for stat in STATS:
             print 'metric %s.%s %s %s' % \
                 (name, stat['key'], stat['unit'], usage[stat['ref']])
+
+        online_nodes = 0
+        offline_nodes = 0
+        draining_nodes = 0
+        total_nodes = len(nodes)
+
+        for node in nodes:
+            if node.status == 'ONLINE':
+                online_nodes = online_nodes + 1
+            elif node.status == 'OFFLINE':
+                offline_nodes = offline_nodes + 1
+            elif node.status == 'DRAINING':
+                draining_nodes = draining_nodes + 1
+
+        print 'metric %s.total_nodes int %s' % (name, total_nodes)
+        print 'metric %s.online_nodes int %s' % (name, online_nodes)
+        print 'metric %s.offline_nodes int %s' % (name, offline_nodes)
+        print 'metric %s.draining_nodes int %s' % (name, draining_nodes)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
