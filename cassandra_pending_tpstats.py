178f46b0e054929b5952f981f3b4b4a87d19fe1b
kaustav haldar <hi@kaustav.me>
2015-10-23 14:38:49 -0700

    fix(status,license,example): Add status messages,licensing info and examples to the plugins

--- cassandra_pending_tpstats.py
+++ cassandra_pending_tpstats.py
@@ -1,5 +1,33 @@
 #!/usr/bin/python
+"""
+Rackspace Cloud Monitoring plugin for pending cassandra tpstats (uses nodetool tpstats)
 
+Example:
+$ ./cassandra_pending_tpstats.py 
+
+Example alarm criteria:
+if (metric['cassandra_pending_foo'] > 10) {
+	return new AlarmStatus(CRITICAL, 'Over 10 pending connections, increase resources')
+}
+
+if (metric['cassandra_pending_foo'] < 5) {
+	return new AlarmStatus(CRITICAL, 'Under 5 pending connections, decrease resources')
+}
+
+Copyright 2015 Rackspace
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 import re
 import socket
 import subprocess
@@ -11,6 +39,7 @@ def camel_to_underscore(name):
 def parse_tpstats(output):
     return re.compile(r'([A-Za-z_]+)\s+\d+\s+(\d+)').findall(output)
 
+
 statsd = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 hostname = socket.gethostname().replace(".", "_")
 
@@ -18,7 +47,9 @@ output, error = subprocess.Popen(['nodetool','tpstats'], stdout=subprocess.PIPE)
 
 if not error:
     for pool_name, count in parse_tpstats(output):
-
         pool_name = camel_to_underscore(pool_name)
 
+        print "status ok"
         print "metric cassandra_pending_{pool} int {count}".format(pool=pool_name, count=count)
+else:
+    print "status err"
---
+++ ssl_cert_expiration.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Rackspace Cloud Monitoring plugin to check if a ssl cert is expired.
+
+# Example:
+# $ ./ssl_cert_expiration.sh <host> <port>
+
+# Example Alarm Criteria:
+# if (metric['cert_end_in'] <= 0) {
+#	return new AlarmStatus(CRITICAL, 'Certificate has expired on host')
+# }
+
+# Copyright 2015 Rackspace
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 <host> <port>"
+  exit 100
+fi
+
+HOST=$1
+PORT=$2
+
+EXPIRATION_DATE=$(echo ""|openssl s_client -connect $HOST:$PORT 2>/dev/null | openssl x509 -noout -enddate | sed 's/^not.*\=//')
+
+REMAINING_SECONDS=$(( $(date -u -d"$EXPIRATION_DATE" +%s) - $(date +%s) ))
+
+echo "status ok"
+echo "metric cert_end_in int ${REMAINING_SECONDS}"
