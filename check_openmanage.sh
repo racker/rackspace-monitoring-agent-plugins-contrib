0c4a0003dfb4f2b568466d930775626c208ff08c
Mark Nipper <nipsy@bitgnome.net>
2016-05-10 16:59:04 -0500

    add check_openmanage.sh plugin (#126)

---
+++ check_openmanage.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# Rackspace Cloud Monitoring Plug-In
+# check_openmanage wrapper plugin to check health of Dell servers via OMSA
+
+# ----------------------------------------------------------------------------
+# "THE BEER-WARE LICENSE" (Revision 42):
+# <nipsy@bitgnome.net> wrote this file.  As long as you retain this notice you
+# can do whatever you want with this stuff. If we meet some day, and you think
+# this stuff is worth it, you can buy me a beer in return.
+# ----------------------------------------------------------------------------
+
+# Depends on Dell's OMSA being installed along with Trond Hasle Amundsen's
+# wonderful check_openmanage plugin for Nagios (avaiable via EPEL on RPM based
+# distributions or directly from his web site:
+# http://folk.uio.no/trondham/software/check_openmanage.html
+
+# Usage:
+# Place plug-in in /usr/lib/rackspace-monitoring-agent/plugins
+#
+# This plugin returns 1 metric:
+#   - status : the exit status returned from the check_openmanage script
+#
+# The following is an example 'criteria' for a Rackspace Monitoring Alarm:
+#
+# :set consecutiveCount=3
+#
+# if (metric['status'] >= 3) {
+#   return new AlarmStatus(CRITICAL, '#{report}');
+# }
+#
+# if (metric['status'] == 2) {
+#   return new AlarmStatus(CRITICAL, '#{report}');
+# }
+#
+# if (metric['status'] == 1) {
+#   return new AlarmStatus(WARNING, '#{report}');
+# }
+#
+# return new AlarmStatus(OK, '#{report}');
+#
+# Things to keep in mind:
+#   - this plugin will try to find check_openmanage in one of the "normal" distribution
+#     managed paths where it might reside, or the actual shell path as a last resort.
+#   - by default, we're ignoring uncertified drive warnings.  Feel free to change that
+#     if it's important in your environment.
+
+opt_args="-b pdisk_cert=all --vdisk-critical"
+
+search=(/usr/lib64/nagios/plugins /usr/lib/nagios/plugins)
+
+for i in ${search[@]}; do
+	if [[ -x ${i}/check_openmanage ]]; then
+		check_cmd="${i}/check_openmanage"
+		break
+	fi
+done
+
+if [[ -z "${check_cmd}" ]]; then
+	if ! check_cmd=$(which check_openmanage); then
+		echo "status Could not find check_openmanage script!"
+		exit 1
+	fi
+fi
+
+report=$(${check_cmd} ${opt_args})
+status=$?
+
+echo "status successfully ran check_openmanage wrapper plugin"
+echo "metric status int32 ${status}"
+echo "metric report string $(echo -E ${report} | head -1)"
+
+exit 0
