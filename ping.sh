3deeed507572e8cfba9d87f65a55ef22f89af5da
Zoltan Ver <kruzda@users.noreply.github.com>
2016-05-12 20:21:31 +0100

    Adding description for ping.sh

--- ping.sh
+++ ping.sh
@@ -1,5 +1,24 @@
 #/usr/sbin/env bash
 
+# Rackspace Cloud Monitoring Plug-In
+# This is a plugin to monitor ICMP response times of hosts accessible by the server
+
+# ----------------------------------------------------------------------------
+# "THE BEER-WARE LICENSE" (Revision 42):
+# <zoltan.ver@gmail.com> wrote this file.  As long as you retain this notice you
+# can do whatever you want with this stuff. If we meet some day, and you think
+# this stuff is worth it, you can buy me a beer in return.
+# ----------------------------------------------------------------------------
+
+# Usage:
+# Place plug-in in /usr/lib/rackspace-monitoring-agent/plugins
+#
+# This plugin returns 4 metrics:
+#   - minimum, average, maximum: statistics returned by the GNU ping utility
+#     in the format "round-trip min/avg/max/stddev = 9.429/35.460/79.698/27.657 ms"
+#   - lost_packets : the percentage of the packets lost out of the number of probes
+#     sent (specifiied by ping_count or the -c command paramter)
+
 target_host="$1"
 ping_count=5
 ping_stats=$(ping -q -w 30 -n -c ${ping_count} ${target_host} 2>&1 | tail -2)
