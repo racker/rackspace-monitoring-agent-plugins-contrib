3be39ef961a70ce4623d13dc405fbd7518bce01a
Zoltan Ver <zoltan.ver@gmail.com>
2016-05-15 12:13:46 +0100

    ping scripts act more like ping checks (1 ping / 2 seconds) also default values (defaults to 5 pings with 2 second intervals) also further fixes here and there

--- ping.sh
+++ ping.sh
@@ -16,28 +16,31 @@ Usage:
 - Ensure that it is executable
   chmod +x ping.sh
 - Configure Custom Plugin type check in Rackspace Intelligence
-  Specify only the script's name and the required parameter(s), e.g.:
-  ping.sh 192.168.0.1
-- Configure an Alert (optional, see example below)
+  Specify only the script's name and the hostname/IP to ping, e.g.:
+  ping.sh 192.168.0.1 <count> <interval>
+  Count is the amount of ICMP probes sent in a singe check, and interval is the
+  number of seconds between them. They are both optional. Their default values
+  are 5 pings with an interval of 2 seconds.
+- Configure an Alert (optional, see example below).
 
 This plugin returns 4 metrics:
   - minimum, average, maximum: statistics returned by the GNU ping utility
     in the format "round-trip min/avg/max/stddev = 9.429/35.460/79.698/27.657 ms"
-  - lost_packets : the percentage of the packets lost out of the number of probes
-    sent (specifiied by ping_count or the -c command paramter)
+  - lost_packets: the percentage of the packets lost out of the number of probes
+    sent in this check run
 
 Example alert:
 
 --- start copying after this line ---
 
-if (metric['average'] >= 15.0) {
-    return new AlarmStatus(CRITICAL, 'Average round-trip takes #{average}ms');
+if (metric['average'] >= 30 ) {
+    return new AlarmStatus(WARNING, 'Average round-trip took #{average}ms');
 }
-if (metric['lost_packets'] >= 30) {
-    return new AlarmStatus(CRITICAL, 'Packet loss is #{lost_packets}%');
+if (metric['lost_packets'] >= 40) {
+    return new AlarmStatus(WARNING, 'Packet loss was #{lost_packets}%');
 }
 if (metric['legacy_state'] != "ok") {
-    return new AlarmStatus(WARNING, 'Error: #{legacy_state}');
+    return new AlarmStatus(CRITICAL, 'Error: #{legacy_state}');
 }
 return new AlarmStatus(OK, 'All good');
 
@@ -45,9 +48,7 @@ return new AlarmStatus(OK, 'All good');
 
 DESCRIPTION
 
-target_host="$1"
-ping_count=5
-ping_stats=$(ping -q -w 30 -n -c ${ping_count} ${target_host} 2>&1 | tail -2)
+ping_stats=$(ping -i "${3:-2}" -q -w 30 -n -c "${2:-5}" "${1}" 2>&1 | tail -2)
 min_ping="$(echo ${ping_stats} | sed -e "s#.\+= \([.0-9]\+\).\+#\\1#g")"
 avg_ping="$(echo ${ping_stats} | cut -d'/' -f5)"
 max_ping="$(echo ${ping_stats} | cut -d'/' -f6)"
--- windows/ping.ps1
+++ windows/ping.ps1
@@ -2,82 +2,90 @@
 
 Rackspace Cloud Monitoring Plug-In
 
-This is a plugin to monitor ICMP round-trip times for hosts that are only
-reachable by the server
+This is a plugin to monitor ICMP response times of hosts accessible by the server
 
 ----------------------------------------------------------------------------
 "THE BEER-WARE LICENSE" (Revision 42):
-<zoltan.ver@gmail.com> wrote this file.  As long as you retain this notice
-you can do whatever you want with this stuff. If we meet some day, and you
-think this stuff is worth it, you can buy me a beer in return.
+<zoltan.ver@gmail.com> wrote this file.  As long as you retain this notice you
+can do whatever you want with this stuff. If we meet some day, and you think
+this stuff is worth it, you can buy me a beer in return.
 ----------------------------------------------------------------------------
 
 Usage:
-
-- Place plug-in into the folder C:\Program Files\Rackspace Monitoring\plugins
+- Place plug-in in folder C:\Program Files\Rackspace Monitoring\plugins
 - Configure Custom Plugin type check in Rackspace Intelligence
-  Specify only the script's name and the required parameter(s), e.g.:
-  ping.ps1 192.168.0.1
-- Configure an Alert (optional, see example below)
+  Specify only the script's name and the hostname/IP to ping, e.g.:
+  ping.ps1 192.168.0.1 <count> <interval>
+  Count is the amount of ICMP probes sent in a singe check, and interval is the
+  number of seconds between them. They are both optional. Their default values
+  are 5 pings with an interval of 2 seconds.
+- Configure an Alert (optional, see example below).
 
 This plugin returns 4 metrics:
   - minimum, average, maximum: statistics returned by the Windows ping utility
-  in the format "Minimum = 0ms, Maximum = 17ms, Average = 4ms"
-  - lost_packets : the percentage of the packets lost out of the number of probes
-  sent (specifiied by the second parameter of this function or the -n command paramter)
+    in the format "Minimum = 0ms, Maximum = 17ms, Average = 4ms
+  - lost_packets: the percentage of the packets lost out of the number of probes
+    sent
 
 Example alert:
 
 --- start copying after this line ---
 
-if (metric['average'] >= 15) {
-    return new AlarmStatus(CRITICAL, 'Average round-trip takes #{average}ms');
+if (metric['average'] >= 30 ) {
+    return new AlarmStatus(WARNING, 'Average round-trip took #{average}ms');
 }
-if (metric['lost_packets'] >= 30) {
-    return new AlarmStatus(CRITICAL, 'Packet loss is #{lost_packets}%');
+if (metric['lost_packets'] >= 40) {
+    return new AlarmStatus(WARNING, 'Packet loss was #{lost_packets}%');
 }
 if (metric['legacy_state'] != "ok") {
-    return new AlarmStatus(WARNING, 'Error: #{legacy_state}');
+    return new AlarmStatus(CRITICAL, 'Error: #{legacy_state}');
 }
 return new AlarmStatus(OK, 'All good');
 
 --- stop copying before this line ---
+
 #>
 
-function CM-Ping {
- param($TargetHost, $count)
-    $ping_command = "ping -n $count -w 30 $TargetHost"
-    $lines = iex $ping_command | select-string "loss|average"
-    Try {
-        $stats_loss = $lines[0]
-        $stats_ping = $lines[1]
-        $loss_percent = "$stats_loss".split("(")[1].split("%")[0]
-        $result_ping = Foreach ($metric in "$stats_ping".split(",")) { $metric.Replace("    Minimum = ", "").Replace(" Maximum = ", "").Replace(" Average = ", "").Replace("ms", "") }
-        $ping_min = $result_ping[0]
-        $ping_max = $result_ping[1]
-        $ping_avg = $result_ping[2]
+function CM-Ping($TargetHost, $count, $interval) {
+    $ping_command = "ping -n 1 -w 30 $TargetHost"
+    $lost_packets=0
+    if (-not $count) { $count = 5 }
+    if (-not $interval ) { $interval = 2 }
+    [int[]] $ping_min, $ping_max, $ping_avg = @()
+    for ($i=0; $i -lt $count; $i++) {
+        $lines = iex $ping_command | select-string "loss|average"
+        if (0 -eq $LASTEXITCODE) {
+            $stats_loss = $lines[0]
+            $stats_ping = $lines[1]
+            if ([int]"$stats_loss".split("(")[1].split("%")[0] -gt 0) {
+                $lost_packets++
+            }
+            $result_ping = Foreach ($metric in "$stats_ping".split(",")) { $metric.Replace("    Minimum = ", "").Replace(" Maximum = ", "").
+Replace(" Average = ", "").Replace("ms", "") }
+            $ping_min += [int]$result_ping[0]
+            $ping_max += [int]$result_ping[1]
+            $ping_avg += [int]$result_ping[2]
+            sleep $interval
+        }
+        else {
+            $lost_packets++
+        }
     }
-    Catch {
-        Write-Output "status err $TargetHost could not be reached"
-        Break
+    if ( $lines ) {
+        Write-Output "metric minimum int32 $(($ping_min | measure -Minimum).Minimum) milliseconds"
+        Write-Output "metric average double $(($ping_min | measure -Average).Average) milliseconds"
+        Write-Output "metric maximum int32 $(($ping_min | measure -Maximum).Maximum) milliseconds"
+        Write-Output "metric lost_packets int32 $([int](([int]$lost_packets / [int]$count) * 100)) percent"
+        Write-Output "status ok"
     }
-    if ( $lines )
-    {
-        Write-Output "metric minimum int32 $ping_min milliseconds"
-        Write-Output "metric average int32 $ping_avg milliseconds"
-        Write-Output "metric maximum int32 $ping_max milliseconds"
-        Write-Output "metric lost_packets int32 $loss_percent percent"
-        Write-Output "status ok host responds"
-    }
-    else
-    {
+    else {
         Write-Output "status err $TargetHost could not be reached"
     }
  }
 
-if($args.Count -lt 1){
-    Write-Output "status err no host specified"
+if($args.Count -lt 1) {
+    Write-Output "status err Missing required parameter"
     exit
 }
 
-CM-Ping -TargetHost $args[0] 5
+CM-Ping -TargetHost $args[0] $args[1] $args[2]
