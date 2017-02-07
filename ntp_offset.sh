a4a68155ebf6eb7389210be60b3768d37f23599a
Michael Burns <michael@mirwin.net>
2017-02-06 16:16:46 -0800

    Update ntp_offset.sh

--- ntp_offset.sh
+++ ntp_offset.sh
@@ -4,7 +4,6 @@
 # Rackspace Cloud Monitoring Plugin to verify the time offset from ntp
 #
 # Copyright (c) 2013, Jordan Evans <jordan.evans@rackspace.com>
-# Copyright (c) 2014, Simon Vetter <simon.vetter@runbox.com>
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -33,26 +32,30 @@
 # This plugin expects to find ntpq and awk in the environment
 # it reports the average ntp offset from ntpq in milliseconds.
 #
-# Example alarm code:
-# :set consecutiveCount=3
+# Example criteria :
 # if (metric['ntp_offset'] > 10000 || metric['ntp_offset'] < -10000) {
 #   return new AlarmStatus(CRITICAL, 'ntp offset is too high.');
 # }
 # return new AlarmStatus(OK, 'ntp offset is fine.');
 #
-# if (metric['active_sources'] < 2) {
-#   return new AlarmStatus(WARNING, 'ntpd is only using #{active_sources} sources');
-# }
-# return new AlarmStatus(OK, 'ntpd has #{active_sources} active sources');
 #
+# if (metric['active_peers'] < 2) {
+#   return new AlarmStatus(WARNING, 'ntpd is only using #{active_peers} peers');
+# }
+# return new AlarmStatus(OK, 'ntpd has #{active_peers} active peers');
 
 NTPQ_BIN=$(which ntpq)
 AWK_BIN=$(which awk)
 
 if [[ -x $NTPQ_BIN ]] && [[ -x $AWK_BIN ]]
   then
-    # only select line starting with * (system peer), + (candidate), # (selected), and o (pps sys peer)
-    OUTPUT=$($NTPQ_BIN -pn | $AWK_BIN '{ if ($1 ~ "^[\\*\\+#o].*" && $9 ~ /[0-9]/) print $9};' | cut -f 1 -d '.')
+    count=0
+    sum=0
+    avg=0
+
+    # only select line starting with * (system peer), o (pps system peer), + (candidate), # (selected)s
+    # and - (outlier).
+    OUTPUT=$($NTPQ_BIN -pn | $AWK_BIN '{ if ($1 ~ "^[\\*\\+#o-].*" && $9 ~ /[0-9]/) print $9};' | cut -f 1 -d '.')
 
     for x in ${OUTPUT}
     do
@@ -64,13 +67,13 @@ if [[ -x $NTPQ_BIN ]] && [[ -x $AWK_BIN ]]
       avg=$(($sum / $count))
       echo "status ok got ntp stats"
       echo "metric ntp_offset int32 ${avg} milliseconds"
-      echo "metric active_sources uint32 ${count} sources"
+      echo "metric active_peers uint32 ${count} peers"
       exit 0
     else
-      echo "status err could not compute ntp offset: no reachable or active source"
+      echo "status could not compute ntp offset: no reachable or active peer"
     fi
   else
-    echo "status err could not compute ntp offset: ntpq and/or awk could not be found"
+    echo "status could not compute ntp offset: ntpq and/or awk could not be found"
 fi
 
 exit 1
