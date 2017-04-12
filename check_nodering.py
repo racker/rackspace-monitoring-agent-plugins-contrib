918520a8f319c9abfc3aea65750c2b0c07c9d528
Jim Wang <jim0476@rackspace.com>
2017-04-12 15:58:17 -0700

    Adding check nodering to contrib directory

---
+++ check_nodering.py
@@ -0,0 +1,26 @@
+#! /usr/bin/python
+
+from subprocess import check_output
+import sys
+import argparse
+
+parser = argparse.ArgumentParser(description='Run nodetool to check for inconsistent state')
+parser.add_argument('-p', '--port', dest='portforcassandra',
+                    default='9080', help='port that cassandra is running on')
+parser.add_argument('-t', '--tool', dest='pathtonodetool',
+                    default='/opt/cassandra/bin/',
+                    help='path to nodetool executable (ex /opt/cassandra/bin)')
+parser.add_argument('-o', '--host', dest='cassandrahost',
+                    default='localhost', help='host cassandra is running on.')
+
+args = parser.parse_args()
+
+nodetooloutput = check_output([args.pathtonodetool + '/nodetool', '-h',
+                               args.cassandrahost, '-p', args.portforcassandra, 'ring'])
+
+if nodetooloutput.find('?') >= 0:
+  print 'CRITICAL nodering not consistent'
+  sys.exit(2)
+else:
+  print 'OK nodering consistent'
+  sys.exit(0)
